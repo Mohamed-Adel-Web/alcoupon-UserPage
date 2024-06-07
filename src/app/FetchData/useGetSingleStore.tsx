902a1f2e93dc723d2356c6087008c1d02e665d66
Mohamed-Adel <151798616+Mohamed-Adel-Web@users.noreply.github.com>
2024-06-08 02:46:44 +0300

    Update useGetSingleStore.tsx

--- src/app/FetchData/useGetSingleStore.tsx
+++ src/app/FetchData/useGetSingleStore.tsx
@@ -5,8 +5,9 @@ const fetchStoreData = async (storeUrl: string): Promise<StoreType> => {
   try {
     const response = await fetch(storeUrl, {
       method: "GET",
-      cache: "no-store",
-    });
+ next: {
+        revalidate: 21600,
+      },    });
 
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
